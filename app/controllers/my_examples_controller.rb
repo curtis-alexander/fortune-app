44a8ecd53b81074b56d29f0ae03e727616a4dd6d
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-06-13 11:11:05 -0500

    add routes for fortune and lotto

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,13 @@
 class MyExamplesController < ApplicationController
+  def random_fortune
+    array = ["Fortune 1", "Fortune 2", "Fortune 3"]
+    fortune = array.sample
+    render json: { message: fortune }
+  end
+
+  def lotto_guess
+    array = (1..60).to_a
+    lotto = array.sample(6)
+    render json: { message: lotto }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/fortune", controller: "my_examples", action: "random_fortune"
+  get "/lotto", controller: "my_examples", action: "lotto_guess"
 end
